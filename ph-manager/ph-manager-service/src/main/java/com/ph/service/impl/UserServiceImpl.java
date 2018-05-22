f4bcdd77660b7bf2b2720b5a3f75e63bed74af4f
fiver <1371408505@qq.com>
2018-05-22 17:06:12 +0800

    第一次提交

---
+++ ph-manager/ph-manager-service/src/main/java/com/ph/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ph.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ph.dao.UserDao;
+import com.ph.model.User;
+import com.ph.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserDao userdao;
+	
+	@Override
+	public List<User> list() {
+		return userdao.list();
+	}
+}
